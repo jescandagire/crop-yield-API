b37d330729ee8a3a078f00bea2e891d1c4e54c1e
NDAGIRE JESCA <47052423+jescandagire@users.noreply.github.com>
2020-12-13 17:05:10 +0300

    completed code

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import json
 import pickle
 import statsmodels.api as sm
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 
 app = Flask(__name__)
@@ -69,26 +70,85 @@ def predict():
     # data_list.append(age_group_youth)
     ##################################################################################
 
+    labelencoder = LabelEncoder()
 
+#for marital status
     marital_status = request_json['marital_status']
+
+    #the way it was encoded in the model
+    marital_status_encoded = {'married': 0, 'single': 1}
+
+    for key in marital_status_encoded:
+    	if key == marital_status:
+    	# if key in marital_status:
+    		marital_status = marital_status_encoded[key]
+
+    #appending to the array
     data_list.append(marital_status)
 
+#for education
     education = request_json['education']
+
+    #the way it was encoded in the model
+    education_encoded = {'primary': 0, 'secondary': 1, 'tertiary': 2}
+
+    for key in education_encoded:
+    	if key == education:
+    	# if key in marital_status:
+    		education = education_encoded[key]
+
+    #appending to the array
     data_list.append(education)
 
+
+#for land ownership
     land_ownership = request_json['land_ownership']
+
+    #the way it was encoded in the model
+    land_ownership_encoded = {'no': 0, 'yes': 1}
+
+    for key in land_ownership_encoded:
+    	if key == land_ownership:
+    		land_ownership = land_ownership_encoded[key]
+
+
+    #appending to the array
     data_list.append(land_ownership)
 
+
+#for farm_size
     farm_size = request_json['farm_size']
+    farm_size = float(farm_size)
+    farm_size = np.log10(farm_size)
     data_list.append(farm_size)
 
+
+#for age group
     age_group = request_json['age_group']
+    
+    if age_group == '18-25':
+    	age_group = 'youth'
+    elif age_group == '26-35':
+    	age_group = 'young_adult'
+    elif age_group == '36-45':
+    	age_group = 'adult'
+    else:
+    	age_group = 'eldery'
+
+
+    #the way it was encoded in the model
+    age_group_encoded = {'adult': 0, 'eldery': 1, 'young_adult': 2, 'youth': 3}
+
+    for key in age_group_encoded:
+    	if key == age_group:
+    		age_group = age_group_encoded[key]
+
+    #appending to the array
     data_list.append(age_group)
     
 
-    print(data_list)
     #changing the string values in the list to float values
-    # x = [float(v) for v in data_list]
+    x = [float(v) for v in data_list]
 
 
 #with the web app
@@ -124,9 +184,10 @@ def predict():
 ########################################################
 
     # load model and predict
-    # model = load_models()
-    # prediction = model.predict(np.array(x))[0]
-    prediction = 19.098765
+    model = load_models()
+    prediction = model.predict(np.array(x))[0]
+    # prediction = 19.098765
+    prediction = 10**prediction
     response = json.dumps({'response': prediction})
 
     return response, 200
